8a1c7f47de2cb4740f156ec61516aabee95cc8f3
SonaghelaKartik <kartik@deepneura.com>
2023-12-10 21:06:29 +0530

    training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.classification_project import logger
 from src.classification_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.classification_project.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.classification_project.pipeline.stage_03_training import ModelTrainingPipeline
 logger.info('welcome to my custom log')
 
 STAGE_NAME = "Data Ingestion stage"
@@ -18,6 +19,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)

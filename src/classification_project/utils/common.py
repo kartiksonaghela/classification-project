520c908111ae21719c09997353d62d269c7c44ca
SonaghelaKartik <kartik@deepneura.com>
2023-12-11 14:10:31 +0530

    dvc added

--- src/classification_project/utils/common.py
+++ src/classification_project/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from src.classification_project import logger
+from classification_project import logger
 import json
 import joblib
 from ensure import ensure_annotations
